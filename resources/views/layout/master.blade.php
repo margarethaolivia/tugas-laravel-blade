ef927d18abce36e5893c557f389b772b76af5db0
Margaretha Olivia <margarethaolivia41@gmail.com>
2020-07-02 08:10:59 +0700

    tugas laravel templating blade selesai

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>@yield('title')</title>
+
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{ asset('/adminLTE/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('/adminLTE/dist/css/adminlte.min.css')}}">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  @include('layout/navbar')
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  @include('layout/sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+  @yield('content')
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  @include('layout/sidebar-content')
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  @include('layout/footer')
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="{{ asset('/adminLTE/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('/adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('/adminLTE/dist/js/adminlte.min.js')}}"></script>
+
+@stack('script')
+
+</body>
+</html>
